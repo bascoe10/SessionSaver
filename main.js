8b7e2a8d314de57e9882c7a8338159cf2fc83aef
Abass Sesay <bassco10@gmail.com>
2019-11-23 12:14:18 -0800

    added all boilerplate files

--- main.js
+++ main.js
@@ -3,4 +3,14 @@ chrome.runtime.onInstalled.addListener(function() {
   chrome.storage.sync.set({ color: "#3aa757" }, function() {
     console.log("The color is green.");
   });
+
+  //specify when popup is shown
+  chrome.declarativeContent.onPageChanged.removeRules(undefined, function() {
+    chrome.declarativeContent.onPageChanged.addRules([
+      {
+        conditions: [new chrome.declarativeContent.PageStateMatcher()],
+        actions: [new chrome.declarativeContent.ShowPageAction()]
+      }
+    ]);
+  });
 });
