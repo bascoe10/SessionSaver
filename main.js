33a670f67aab55363886d0952bd5973ef6cdfeb2
Abass Sesay <bassco10@gmail.com>
2019-11-23 11:34:38 -0800

    add html file for popup

---
+++ main.js
@@ -0,0 +1,6 @@
+// registering a listener after installing the extension
+chrome.runtime.onInstalled.addListener(function() {
+  chrome.storage.sync.set({ color: "#3aa757" }, function() {
+    console.log("The color is green.");
+  });
+});
