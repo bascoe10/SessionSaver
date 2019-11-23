b74485a614e18501cd1f3632c544a92b0c310ec8
Abass Sesay <bassco10@gmail.com>
2019-11-23 12:54:31 -0800

    user can specify session name

--- popup.js
+++ popup.js
@@ -1,5 +1,20 @@
 let saveBtn = document.getElementById("saveSession");
 
-saveBtn.onclick = function() {
-  console.log("Saving");
+saveBtn.onclick = () => {
+  let sessionName = getSessionName();
+  chrome.tabs.query({ currentWindow: true }, tabs => {
+    let tabUrls = tabs.map(tab => tab.url);
+    console.log(tabUrls);
+    console.log(sessionName);
+  });
+};
+
+const getSessionName = () => {
+  let defaultName = getDefaultSessionName();
+  let sessionName = prompt("Enter Name", defaultName);
+  return sessionName;
+};
+
+const getDefaultSessionName = () => {
+  return `Session @ ${new Date()}`;
 };
