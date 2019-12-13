ae41fad12e60493271089db43a92331fdc24e7a9
Abass Sesay <bassco10@gmail.com>
2019-12-12 20:06:44 -0800

    refactor session store to index sessions by timestamp and then store an object with name time and tabs

--- popup.js
+++ popup.js
@@ -1,30 +1,18 @@
 const saveBtn = document.getElementById("saveSession");
 const options = ["Restore", "Delete"];
-
-const createLiNode = value => {
-  node = document.createElement("li");
-  text = document.createTextNode(value);
-  node.appendChild(text);
-
-  options.forEach(opt => {
-    optNode = document.createElement("a");
-    textNode = document.createTextNode(opt);
-    optNode.appendChild(textNode);
-    optNode.setAttribute("class", "option");
-
-    node.appendChild(optNode);
-  });
-
-  console.log(node);
-  return node;
-};
+let nextSessionIndex = 1;
 
 chrome.storage.sync.get("SessionSaverSessions", data => {
   let sessionStore = data["SessionSaverSessions"];
   let savedSessionsSection = document.getElementById("savedSessions");
+  let keys = [];
+
   if (sessionStore) {
-    Object.keys(sessionStore).forEach(entry => {
-      node = createLiNode(entry);
+    keys = Object.keys(sessionStore);
+    nextSessionIndex = keys.length + 1;
+
+    keys.forEach(entry => {
+      node = createLiNode(sessionStore[entry].name);
       savedSessionsSection.appendChild(node);
     });
   }
@@ -38,32 +26,52 @@ saveBtn.onclick = () => {
   });
 };
 
-const getSessionName = () => {
+function getSessionName() {
   let defaultName = getDefaultSessionName();
-  let sessionName = prompt("Enter Name", defaultName);
+  let sessionName = prompt("Enter Name", defaultName) || defaultName;
   return sessionName;
-};
+}
 
-const getDefaultSessionName = () => {
-  return `Session @ ${new Date()}`;
-};
+function getDefaultSessionName() {
+  return `Session - ${nextSessionIndex}`;
+}
 
-const storeSession = (name, urls) => {
+function storeSession(name, urls) {
   let savedSessionsSection = document.getElementById("savedSessions");
   let node = createLiNode(name);
   chrome.storage.sync.get("SessionSaverSessions", function(data) {
     let sessionStore = data["SessionSaverSessions"];
+    let timestamp = Date.now();
     if (!sessionStore) {
       sessionStore = {};
     }
-    sessionStore[name] = urls;
+
+    sessionStore[timestamp] = { name, timestamp, urls };
     chrome.storage.sync.set(
       {
         SessionSaverSessions: sessionStore
       },
       function() {
         savedSessionsSection.appendChild(node);
+        nextSessionIndex += 1;
       }
     );
   });
-};
+}
+
+function createLiNode(value) {
+  node = document.createElement("li");
+  text = document.createTextNode(value);
+  node.appendChild(text);
+
+  options.forEach(opt => {
+    optNode = document.createElement("a");
+    textNode = document.createTextNode(opt);
+    optNode.appendChild(textNode);
+    optNode.setAttribute("class", "option");
+
+    node.appendChild(optNode);
+  });
+
+  return node;
+}
